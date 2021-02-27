f7aba868a6737be0fbfe45e6ed12ba84b99c4efa
Sodiq Afolayan <sodiq.o.afolayan@gmail.comgit config --global user.email sodiq.o.afolayan@gmail.com>
2021-02-27 09:37:26 -0600

    leetcode simple add sum

--- day20_validpair.py
+++ day20_validpair.py
@@ -16,3 +16,19 @@ def ValidPair(self, a, n):
     	c = [1 for (k,v) in combinations(a, 2) if k != v and k + v > 0]
         return sum(c)
 
+import os
+
+path = "path to file"
+df_list = []
+for filename in os.listdir(path):
+    if filename.endswith(".csv"): # This ensures that the code only read csv files
+        filename_list = filename.split(".")
+        # filename_list = filename.split(".") when split() is used on string, it turns it to list. Hence we have filename_list = ["file_name", "csv"] for each file name
+        df_name = filename_list[0]
+        df = spark.read.csv(path+filename, inferSchema=True, header=True) # filename here will reference the actual filname.csv before we splitted it
+        df.name =df_name # after reading in each csv in the immediate above code, this line renames it to its own name. Hence df above is a temporary place holder for each dataframe
+        df_list.append(df_name) # we are just appending the names of the dataframe here, nothing more
+        exec(df_name + " = df")
+
+# df_list
+
---
+++ day21_leetcode2numbers.py
@@ -0,0 +1,13 @@
+# LEETCODE https://leetcode.com/problems/two-sum/
+
+ Given an array of integers nums and an integer target, return indices of the two 
+# numbers such that they add up to target. You may assume that each input would have 
+# exactly one solution, and you may not use the same element twice. You can return the 
+# answer in any order.
+
+from itertools import combinations
+nums = [2,7,11,15]
+target = 9
+for item in (combinations(nums, 2)):
+    if sum(item) == target:
+        print([nums.index(item[0]), nums.index(item[-1])])

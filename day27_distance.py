6bfc9ec88f42443fa5d9f0648d131487b7bde3c8
Sodiq Afolayan <sodiq.o.afolayan@gmail.comgit config --global user.email sodiq.o.afolayan@gmail.com>
2021-03-02 11:28:23 -0600

    updated distance funtion

--- day27_distance.py
+++ day27_distance.py
@@ -10,22 +10,22 @@ and returns the distance between those points as a float.
 def distance(x1, y1, x2, y2):
     # Get change in x axis
     change_in_x = x2 - x1 
-    
+    print(change_in_x)
     # Square the difference
     change_in_x_squared = change_in_x ** 2
-    
+    print(change_in_x_squared)
     # Get change in y axis
     change_in_y = y2 - y1 
-    
+    print(change_in_y)
     # Square the difference 
     change_in_y_squared = change_in_y ** 2
-    
+    print(change_in_y_squared)
     # Add change_in_x_squared and change_in_y_squared
     sum_change_x_y_squared = change_in_x_squared + change_in_y_squared
-    
+    print(sum_change_x_y_squared)
     # Get the square root of sum_change_x_y_squared
     square_root = sqrt(change_in_x_squared + change_in_y_squared)
-    
+    print(square_root)
     # Return the int value
     return int(square_root)
 
@@ -37,11 +37,13 @@ def distance2(x1, y1, x2, y2):
 
 
 
-print(distance(0, 0, 3, 4))
-print(distance(-1, -2, 3, 1))
-print(distance(-.5, .5, .5, -.5))
+#print(distance(0, 0, 3, 4))
+#print(distance(-1, -2, 3, 1))
+#print(distance(-.5, .5, .5, -.5))
+print(distance(5, 3, 7, 9))
 
-print(distance2(0, 0, 3, 4))
-print(distance2(-1, -2, 3, 1))
-print(distance2(-.5, .5, .5, -.5))
+#print(distance2(0, 0, 3, 4))
+#print(distance2(-1, -2, 3, 1))
+#print(distance2(-.5, .5, .5, -.5))
+print(distance2(5, 3, 7, 9))
    

ca2e868224e0e8b357fe5805691b439a00d07e78
Sodiq Afolayan <sodiq.o.afolayan@gmail.comgit config --global user.email sodiq.o.afolayan@gmail.com>
2021-03-19 19:35:03 -0500

    total shares as a function

--- day35_pcost.py
+++ day35_pcost.py
@@ -20,3 +20,22 @@ with open("Data/portfolio.csv", "r") as portfolio:
         totalSharesCost = totalSharesCost + (shares*price)
 
 print(totalSharesCost)
+
+
+def totalShares(file):
+    with open(file, "r") as portfolio:
+        # next() will return same file but from the next line
+        portfolioHeader = next(portfolio).split(',')
+        # This is a placeholder for the current totalSharesCost
+        totalSharesCost = 0.0
+        for row in portfolio:
+            # This turns each row into a list
+            row = row.split(',')
+            # I already know the data i need are index 1 and 2
+            shares = int(row[1])
+            price = float(row[2]) # using int() gave me error: ValueError: invalid literal for int() with base 10: '32.20\n'
+            totalSharesCost = totalSharesCost + (shares*price)
+    return totalSharesCost
+#print(totalSharesCost)
+
+print(totalShares("Data/portfolio.csv"))

772cecdb8718d628097709962145fd246011efb6
Sodiq Afolayan <sodiq.o.afolayan@gmail.comgit config --global user.email sodiq.o.afolayan@gmail.com>
2021-04-18 12:32:42 -0500

    is prime detailed updated

--- day45_is_prime.py
+++ day45_is_prime.py
@@ -1,9 +1,10 @@
 from math import sqrt
 
 # Helper function to be used in is_prime() function
+"""
 def square_root(n: int) -> int:
     return sqrt(n)
-
+"""
 
 def is_prime(n: int) -> bool:
     # Prime numbers are natural numbers > 1 and not a product of two other natural numbers (eg 3, 5, 7, 11 etc)
@@ -16,17 +17,18 @@ def is_prime(n: int) -> bool:
     # than the square root and the other not smaller than the square root of 24
     # Hence, we only need to iterate from 3 to the squaroot of the number we want to check if its prime or not to the number + 1
     # skipping even numbers
-    max_factor = round(square_root(n)) 
-    for factor in range(3, max_factor+1, 2):
+    # max_factor = round(square_root(n)) 
+    for factor in range(3, round(sqrt(n) + 1), 2):
         if n % factor == 0:
             return False
     return True
-
+"""
 def test_square_root():
     print("Testing square root function....")
     assert(square_root(100) == 10)
     assert(square_root(4) == 2)
     print("Passed......")
+"""
 
 def test_is_prime():
     print("Testing is_prime function.....")
@@ -37,5 +39,5 @@ def test_is_prime():
     print("Passed......")
 
 
-test_square_root()
+# test_square_root()
 test_is_prime()
